b99133ad65695c89cb640afe0d1e815f392199e5
Oscar Otero <oom@oscarotero.com>
2023-07-01 17:33:42 +0200

    cname

--- docs/_config.ts
+++ docs/_config.ts
@@ -1,7 +1,6 @@
 import lume from "lume/mod.ts";
 import wiki from "https://deno.land/x/lume_theme_simple_wiki@v0.1.1/mod.ts";
 
-const site = lume();
-site.use(wiki());
-
-export default site;
+export default lume({ location: new URL("https://vento.js.org") })
+  .use(wiki())
+  .copy("CNAME");
