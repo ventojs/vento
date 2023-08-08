6a9b2b72d45da34809f93c6abaa4b57cec26ca48
Oscar Otero <oom@oscarotero.com>
2023-08-08 18:20:34 +0200

    updated docs theme

--- docs/_config.ts
+++ docs/_config.ts
@@ -1,5 +1,5 @@
 import lume from "lume/mod.ts";
-import wiki from "https://deno.land/x/lume_theme_simple_wiki@v0.2.0/mod.ts";
+import wiki from "https://deno.land/x/lume_theme_simple_wiki@v0.4.0/mod.ts";
 
 export default lume({ location: new URL("https://vento.js.org") })
   .use(wiki())
