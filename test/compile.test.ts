717edaa86aeaf822304f16c38c1f22d651f83fe8
Oscar Otero <oom@oscarotero.com>
2023-09-02 17:45:04 +0200

    test escaped backtick #12

--- test/compile.test.ts
+++ test/compile.test.ts
@@ -13,4 +13,10 @@ Deno.test("Strings with dangerous characters", async () => {
     expected:
       '<h1>Test</h1><script>let name = "Gandalf"; console.log(`Hi ${name}.`);</script>',
   });
+  await test({
+    template:
+      '<h1>Test</h1><script>let name = "Gandalf"; console.log(`Hi \`${name}\`.`);</script>',
+    expected:
+      '<h1>Test</h1><script>let name = "Gandalf"; console.log(`Hi \`${name}\`.`);</script>',
+  });
 });
