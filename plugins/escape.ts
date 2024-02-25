de7cf76033cf726492a96aa9a6ec3ebf9411449d
wrapperup <wrapperup4@gmail.com>
2024-02-24 19:47:23 -0500

    emulate non-escaped behavior

--- plugins/escape.ts
+++ plugins/escape.ts
@@ -4,6 +4,7 @@ import type { Environment } from "../src/environment.ts";
 export default function () {
   return (env: Environment) => {
     // deno-lint-ignore no-explicit-any
-    env.filters.escape = (value: any) => html.escape(value.toString());
+    env.filters.escape = (value: any) => value ? html.escape(value.toString()) : "";
   };
 }
+
