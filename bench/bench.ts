d022122918265a2c859f015561f2330209b4878b
Oscar Otero <oom@oscarotero.com>
2024-07-03 23:28:35 +0200

    updated deps

--- bench/bench.ts
+++ bench/bench.ts
@@ -1,6 +1,6 @@
 import vento from "../mod.ts";
 import nunjucks from "npm:nunjucks@3.2.4";
-import { Liquid } from "npm:liquidjs@10.13.1";
+import { Liquid } from "npm:liquidjs@10.14.0";
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 
 const env = vento({
