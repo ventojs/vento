9144a7220cc30b1782b474bb3dd606b743023add
Oscar Otero <oom@oscarotero.com>
2025-02-02 12:16:58 +0100

    install lua language

--- docs/_config.ts
+++ docs/_config.ts
@@ -1,7 +1,8 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
 
-import "npm:prismjs@1.29.0/components/prism-markup-templating.js";
+import "npm:prismjs/components/prism-markup-templating.js";
+import "npm:prismjs/components/prism-lua.js";
 import "../prism-vento.js";
 
 export default lume({ location: new URL("https://vento.js.org") })
