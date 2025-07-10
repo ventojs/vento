b3f7fa6d61a3cee16c888c23d35d7950704b047f
vrugtehagel <vrugtehagel@pm.me>
2025-07-10 18:12:37 +0200

    Add tokenizer and top-level-iterating benchmarks
    I've added a variety of different large scripts for the top-level
    iteration benchmark, because I noticed their performance profile is all
    quite different. Some files include a lot of regexes, some a lot of
    escaping within strings, etcetera. Having a variety of benchmarks helps
    catch edge cases.

---
+++ bench/tokenizer.bench.ts
@@ -0,0 +1,13 @@
+import tokenizerMain from 'https://raw.githubusercontent.com/ventojs/vento/refs/heads/main/src/tokenizer.ts';
+import tokenizerNext from '../src/tokenizer.ts';
+
+const url = new URL('./assets/lorem-ipsum.txt', import.meta.url);
+const source = await Deno.readTextFile(url);
+
+Deno.bench("Vento's main branch", () => {
+	tokenizerMain(source);
+})
+
+Deno.bench("Current branch", () => {
+	tokenizerMain(source);
+})
