0af1f7d7bbaff54afe9616e4be1f06c24e5f6c8a
Oscar Otero <oom@oscarotero.com>
2025-09-04 21:59:26 +0200

    fixed broken links

--- docs/_config.ts
+++ docs/_config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
+import checkUrls from "lume/plugins/check_urls.ts";
 
 import "npm:prismjs/components/prism-markup-templating.js";
 import "npm:prismjs/components/prism-lua.js";
@@ -7,4 +8,5 @@ import "../prism-vento.js";
 
 export default lume({ location: new URL("https://vento.js.org") })
   .use(wiki())
+  .use(checkUrls())
   .copy("CNAME");
