95b68b15c5551c491eed907f7ff1dd25aa8e6fa0
vrugtehagel <vrugtehagel@pm.me>
2025-09-13 09:42:21 +0200

    Fix version-less imports
    Probably we should really pin dependencies given things can break in
    spectacular ways if we just accept any (even major) changes to a
    package.

--- docs/_config.ts
+++ docs/_config.ts
@@ -2,8 +2,8 @@ import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
 import checkUrls from "lume/plugins/check_urls.ts";
 
-import "npm:prismjs/components/prism-markup-templating.js";
-import "npm:prismjs/components/prism-lua.js";
+import "npm:prismjs@1.30.0/components/prism-markup-templating.js";
+import "npm:prismjs@1.30.0/components/prism-lua.js";
 import "../prism-vento.js";
 
 export default lume({ location: new URL("https://vento.js.org") })
