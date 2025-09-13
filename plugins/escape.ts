325efef7c6cfa5e642b172b3cccdc7c1a0c7e1e0
vrugtehagel <vrugtehagel@pm.me>
2025-09-13 09:33:05 +0200

    Fix and simplify escaping of JSON (and other)
    The loop when escaping starts at the first index of a character that
    needs to be escaped. In the tests, this ends up always being the first
    characters, since we're testing something that starts with `<h1>`. For
    JSON it's generally the second character, `"`, that needs escaping, and
    so the first character gets skipped.
    
    I've also taken the liberty of removing the loop entirely and replacing
    it with a much simpler `.replace()` call.

--- plugins/escape.ts
+++ plugins/escape.ts
@@ -1,7 +1,14 @@
 import { SafeString } from "../core/environment.ts";
 import type { Environment, Plugin } from "../core/environment.ts";
 
-const UNSAFE = /[<>"&']/;
+const UNSAFE = /[<>"&']/g;
+const ESCAPES: Record<string, string> = {
+  '<': '&lt;',
+  '>': '&gt;',
+  '\'': '&apos;',
+  '"': '&quot;',
+  '&': '&amp;',
+}
 
 export default function (): Plugin {
   return (env: Environment) => {
@@ -11,37 +18,7 @@ export default function (): Plugin {
 
       const str = value.toString();
 
-      const match = UNSAFE.exec(str);
-
-      if (match === null) {
-        return str;
-      }
-
-      let html = "";
-
-      for (let i = match.index; i < str.length; i++) {
-        switch (str.charCodeAt(i)) {
-          case 34: // "
-            html += "&quot;";
-            break;
-          case 39: // '
-            html += "&apos;";
-            break;
-          case 38: // &
-            html += "&amp;";
-            break;
-          case 60: // <
-            html += "&lt;";
-            break;
-          case 62: // >
-            html += "&gt;";
-            break;
-          default:
-            html += str[i];
-        }
-      }
-
-      return html;
+      return str.replace(UNSAFE, match => ESCAPES[match])
     };
   };
 }
